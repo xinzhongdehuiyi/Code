7fc9140c0b56fd91fe868d8b71ac5eb06d001881
CCA <xinzhongdehuiyi@gmail.com>
2019-08-10 09:17:15 +0800

    Frist Commit...

--- app/src/main/java/com/app/code/MainActivity.java
+++ app/src/main/java/com/app/code/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViewById(R.id.btn).setOnClickListener(v -> {
-
+            //1
         });
     }
 }
